cc580a5aac3ab0f99839f90528f008a2a52db93b
BSH <tmdghks605@naver.com>
2023-04-26 17:40:16 +0900

    완료

--- sleepapp/models.py
+++ sleepapp/models.py
@@ -2,36 +2,34 @@ from django.db import models
 
 class Sleep(models.Model):
     
-    #id = models.AutoField(primary_key=True)
-
-    Colunm_1 = models.CharField(max_length=20)
-    Colunm_2 = models.CharField(max_length=20)
-    Colunm_3 = models.CharField(max_length=20)
-    Colunm_4 = models.CharField(max_length=20)
-    Colunm_5 = models.CharField(max_length=20)
-    Colunm_6 = models.FloatField()
-    Colunm_7 = models.CharField(max_length=20)
-    Colunm_8 = models.CharField(max_length=40)
-    Colunm_9 = models.CharField(max_length=40)
-    Colunm_10 = models.FloatField()
-    Colunm_11 = models.FloatField()
-    Colunm_12 = models.CharField(max_length=20)
-    Colunm_13 = models.FloatField()
-    Colunm_14 = models.CharField(max_length=20)
-    Colunm_15 = models.CharField(max_length=20)
-    Colunm_16 = models.CharField(max_length=20)
-    Colunm_17 = models.CharField(max_length=20)
-    Colunm_18 = models.CharField(max_length=20)
-    Colunm_19 = models.CharField(max_length=20)
-
+    id = models.AutoField(primary_key=True)
+    졸음쉼터명 = models.CharField(max_length=20)
+    시도명 = models.CharField(max_length=20)
+    시군구명 = models.CharField(max_length=20)
+    도로종류 = models.CharField(max_length=20)
+    도로노선명 = models.CharField(max_length=20)
+    도로노선번호 = models.FloatField()
+    도로노선방향 = models.CharField(max_length=20)
+    소재지도로명주소 = models.CharField(max_length=40)
+    소재지지번주소 = models.CharField(max_length=40)
+    위도 = models.FloatField()
+    경도 = models.FloatField()
+    총연장 = models.CharField(max_length=20)
+    주차면수 = models.FloatField()
+    화장실유무 = models.CharField(max_length=20)
+    방범용CCTV수 = models.CharField(max_length=20)
+    기타편의시설 = models.CharField(max_length=20)
+    관리기관명 = models.CharField(max_length=20)
+    관리기관전화번호 = models.CharField(max_length=20)
+    데이터기준일자 = models.CharField(max_length=20)
 
     class Meta:
         db_table = 'sleep'
         managed = False
 
-# #회원 정보
-# class User(models.Model):
-#     email = models.CharField(max_length=20)
-#     name = models.CharField(max_length=20)
-#     pwd = models.CharField(max_length=20)
-#     c_date = models.DateTimeField()
+#회원 정보
+class User(models.Model):
+    email = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    pwd = models.CharField(max_length=20)
+    c_date = models.DateTimeField()
--- sleepapp/views.py
+++ sleepapp/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Sleep
+from .models import Sleep, User
+from django.utils import timezone
+import hashlib
 
 def main(request):
     sleep = Sleep.objects.all()
 
     return render(request, 'sleepapp/main.html', {'sleep': sleep})
+
+def user_signup(request):
+    #POST 로 요청했을 때
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        name = request.POST.get('name')
+        pwd = request.POST.get('pwd')
+        u = User()
+        u.email = email
+        u.name = name
+        u.pwd = encrypt(pwd)
+        u.c_date = timezone.now()
+        u.save()
+
+        return HttpResponse('%s님 가입 감사합니다.' % name)
+    #GET 으로 요청했을 때
+    return render(request, 'firstapp/signup.html', {})
+
+def encrypt(pwd):
+    m = hashlib.sha256()
+    m.update(pwd.encode('utf-8'))
+    new_pwd = m.hexdigest()
+    return new_pwd
